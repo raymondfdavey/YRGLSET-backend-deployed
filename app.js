4048dc55b3d5ec3e23e242620838cafe72ef884d
raymondfdavey <raymondfdavey@gmail.com>
2020-04-10 14:51:58 +0100

    few changes stillsza

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const apiRouter = require("./routes/api.router");
-const cors = require("cors");
+var cors = require("cors");
 const app = express();
 
 app.use(cors());
@@ -8,13 +8,5 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
-
-// app.use((err, req, res, next) => {
-//   if (err.code === "22P02") {
-//     res.status(400).send({ msg: "bad request" });
-//   } else {
-//     res.status(err.status || 500).send({ msg: err.msg || "sever fail" });
-//   }
-// });
 
 module.exports = app;
--- routes/api.router.js
+++ routes/api.router.js
@@ -1,7 +1,5 @@
 const apiRouter = require("express").Router();
 const tasksRouter = require("./tasks.router.js");
-const cors = require("cors");
-app.use(cors());
 
 apiRouter.get("/", (req, res) => console.log("getting to apiRouter"));
 
